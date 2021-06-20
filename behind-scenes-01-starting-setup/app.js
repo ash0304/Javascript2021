76fbfe6495c42e230da04d3a0dd495c635d40453
Ash Lai <z0975201508@gmail.com>
2021-06-20 14:51:28 +0800

    Section 5: course 113. var vs let & const - introducing 'block scope'

---
+++ behind-scenes-01-starting-setup/app.js
@@ -0,0 +1,17 @@
+var name = 'Ash';
+let hobbies;
+
+if (name === 'Ash') {
+  hobbies = ['Music', 'Swimming'];
+  console.log(hobbies);
+}
+
+function greet() {
+  var age = 27;
+  var name = 'Manuel';
+  console.log(name, age, hobbies);
+}
+
+console.log(name, hobbies);
+
+greet();
