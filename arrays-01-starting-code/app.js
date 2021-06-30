3080f89a795e0ae3b91558df6ee39ed900af63f1
Ash Lai <z0975201508@gmail.com>
2021-06-30 16:16:22 +0800

    course 195. Transforming Data with map()

--- arrays-01-starting-code/app.js
+++ arrays-01-starting-code/app.js
@@ -77,18 +77,30 @@
 
 // console.log(ashIndex);
 
+// const prices = [10.99, 5.99, 3.99, 6.59];
+// const tax = 0.19;
+
+// const taxAdjustedPrices = [];
+
+// // for (const price of prices) {
+// //   taxAdjustedPrices.push(price * (1 + tax));
+// // }
+
+// prices.forEach((price, idx, prices) => {
+//   const priceObj = { index: idx, taxAdjustedPrices: price * (1 + tax) };
+//   taxAdjustedPrices.push(priceObj);
+// });
+
+// console.log(taxAdjustedPrices);
+
 const prices = [10.99, 5.99, 3.99, 6.59];
 const tax = 0.19;
 
-const taxAdjustedPrices = [];
-
-// for (const price of prices) {
-//   taxAdjustedPrices.push(price * (1 + tax));
-// }
-
-prices.forEach((price, idx, prices) => {
-  const priceObj = { index: idx, taxAdjustedPrices: price * (1 + tax) };
-  taxAdjustedPrices.push(priceObj);
+const taxAdjustedPrices = prices.map((price, idx) => {
+  return {
+    index: idx,
+    taxAdjPrice: price * (1 + tax),
+  };
 });
 
-console.log(taxAdjustedPrices);
+console.log(prices, taxAdjustedPrices);
