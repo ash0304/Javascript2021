8dbb352366348054aba8d5a4dbfbde03f89b77d8
Ash Lai <z0975201508@gmail.com>
2021-06-30 16:23:43 +0800

    course 196. sort() and reverse()

--- arrays-01-starting-code/app.js
+++ arrays-01-starting-code/app.js
@@ -103,4 +103,17 @@ const taxAdjustedPrices = prices.map((price, idx) => {
   };
 });
 
-console.log(prices, taxAdjustedPrices);
+// console.log(prices, taxAdjustedPrices);
+
+const sortedPrices = prices.sort((a, b) => {
+  console.log(a, b);
+  if (a > b) {
+    return 1;
+  } else if (a === b) {
+    return 0;
+  } else {
+    return -1;
+  }
+});
+// console.log(sortedPrices.reverse());
+console.log(sortedPrices);
