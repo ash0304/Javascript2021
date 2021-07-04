d7a060959985102301cfa1820be7b51cfe288bc8
Ash Lai <z0975201508@gmail.com>
2021-07-04 17:09:05 +0800

    course 208. Understanding WeakSet

--- arrays-01-starting-code/setsmap.js
+++ arrays-01-starting-code/setsmap.js
@@ -9,22 +9,31 @@
 //   console.log(entry);
 // }
 
-const person1 = { name: 'Ash' };
-const person2 = { name: 'Anna' };
+// const person1 = { name: 'Ash' };
+// const person2 = { name: 'Anna' };
 
-const personData = new Map([[person1, [{ date: 'yesterday', price: 10 }]]]);
+// const personData = new Map([[person1, [{ date: 'yesterday', price: 10 }]]]);
 
-personData.set(person2, [{ date: 'two weeks ago', price: 100 }]);
+// personData.set(person2, [{ date: 'two weeks ago', price: 100 }]);
 
-// console.log(personData);
-// console.log(personData.get(person1));
+// // console.log(personData);
+// // console.log(personData.get(person1));
 
-for (const [key, value] of personData.entries()) {
-  console.log(key, value);
-}
+// for (const [key, value] of personData.entries()) {
+//   console.log(key, value);
+// }
 
-for (const key of personData.keys()) {
-  console.log(key);
-}
+// for (const key of personData.keys()) {
+//   console.log(key);
+// }
 
-console.log(personData.size);
+// console.log(personData.size);
+
+let person = { name: 'Ash' };
+const persons = new WeakSet();
+persons.add(person);
+
+// ...  some operations;
+person = null;
+
+console.log(persons);
