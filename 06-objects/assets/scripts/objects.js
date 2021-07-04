ff280767f76b2ff4e6c0f85c32c0836f0db1b398
Ash Lai <z0975201508@gmail.com>
2021-07-05 02:19:13 +0800

    course 217. Special Key Names & Square Bracket Property Access

--- 06-objects/assets/scripts/objects.js
+++ 06-objects/assets/scripts/objects.js
@@ -1,5 +1,11 @@
+const movieList = document.getElementById('movie-list');
+
+// movieList.style.backgroundColor = 'red';
+movieList.style['background-color'] = 'red';
+movieList.style.display = 'block';
+
 let person = {
-  name: 'Ash',
+  'first-name': 'Ash',
   age: 27,
   hobbies: ['Music', 'Swimming'],
   greet: function () {
@@ -15,6 +21,4 @@ let person = {
 person.age = null;
 person.isAdmin = true;
 
-
-
-console.log(person);
+console.log(person['first-name']);
