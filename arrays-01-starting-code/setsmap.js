e976ac4b9c5f941d0a3beb11595467bc3748922b
Ash Lai <z0975201508@gmail.com>
2021-07-04 16:52:50 +0800

    course 206. Working with Maps

--- arrays-01-starting-code/setsmap.js
+++ arrays-01-starting-code/setsmap.js
@@ -8,3 +8,23 @@
 // for (const entry of ids.values()) {
 //   console.log(entry);
 // }
+
+const person1 = { name: 'Ash' };
+const person2 = { name: 'Anna' };
+
+const personData = new Map([[person1, [{ date: 'yesterday', price: 10 }]]]);
+
+personData.set(person2, [{ date: 'two weeks ago', price: 100 }]);
+
+// console.log(personData);
+// console.log(personData.get(person1));
+
+for (const [key, value] of personData.entries()) {
+  console.log(key, value);
+}
+
+for (const key of personData.keys()) {
+  console.log(key);
+}
+
+console.log(personData.size);
